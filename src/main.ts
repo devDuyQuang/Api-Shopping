81f8e9d8554c1ded5442725c04e267574a3e681d
võ duy quảng <voduyquang@MacBook-Air-cua-vo.local>
2026-01-20 16:39:24 +0700

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -29,6 +29,12 @@ import { ResponseInterceptor } from './common/interceptors/response.interceptor'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['https://voduyquang.com'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // chỉ bật nếu bạn dùng cookie
+  });
 
   app.enableCors({
     origin: '*',
